181cf778a18f2725fd1486fdf734056febc993a7
26583 <you@example.com26583@ma-web.nl>
2020-05-07 12:13:16 +0200

    Beweging

---
+++ Mythe Project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody rb;
+    float defualtSpeed = 5f;
+    float speed = 5;
+    float mouseSpeed = 20;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 dir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        dir.Normalize();
+            transform.position += transform.forward * dir.z * speed * Time.deltaTime;
+            transform.position += transform.right * dir.x * speed * Time.deltaTime;
+        Vector3 rotation = transform.eulerAngles;
+
+        rotation.y += Input.GetAxis("Mouse X") * mouseSpeed * Time.deltaTime; // Standart Left-/Right Arrows and A & D Keys
+
+        transform.eulerAngles = rotation;
+    }
+}
