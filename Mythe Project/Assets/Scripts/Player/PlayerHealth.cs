feffe5a74709d5fa82187c19eaca9605a9fe11a6
Wayne Hofstra <wayne.hofstra@hva.nl>
2020-05-08 17:42:35 +0200

    Added Player Health

--- Mythe Project/Assets/Scripts/Player/PlayerHealth.cs
+++ Mythe Project/Assets/Scripts/Player/PlayerHealth.cs
@@ -1,16 +1,61 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class PlayerHealth : MonoBehaviour
 {
+    [SerializeField] int _maximumHealth;
+
+    public event Action changeHealth;
+
+    int currentHealth;
+    bool hit;
+    
+    public int CurrentHealth { get { return currentHealth; } set { currentHealth = value; } }
+
     void Start()
     {
-
+        currentHealth = _maximumHealth;
+        hit = false;
+        changeHealth();
     }
 
-    void Update()
+    void OnCollisionStay(Collision collision)
     {
+        if (collision.gameObject.layer.Equals(9))
+        {
+            GetHit(9);
+            hit = true;
+            StartCoroutine(HitCoroutine());
+        }
+    }
 
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.layer.Equals(9))
+        {
+            hit = true;
+            StartCoroutine(HitCoroutine());
+        }
+    }
+
+    IEnumerator HitCoroutine()
+    {
+        yield return new WaitForSeconds(0.5f);
+        hit = false;
+    }
+
+    void GetHit(int damage)
+    {
+        if (!hit){
+            if (currentHealth > damage) {
+                currentHealth -= damage;
+            }
+            else {
+                currentHealth = 0;
+            }
+            changeHealth();
+        }
     }
 }
---
+++ Mythe Project/Assets/Scripts/UI/PlayerStatDisplay.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerStatDisplay : MonoBehaviour
+{
+    Text text;
+    PlayerHealth playerHealth;
+    RectTransform transform;
+
+    void Start()
+    {
+        text = GetComponent<Text>();
+        playerHealth = FindObjectOfType<PlayerHealth>();
+        transform = GetComponent<RectTransform>();
+
+        if (playerHealth != null)
+        {
+            playerHealth = playerHealth.GetComponent<PlayerHealth>();
+            playerHealth.changeHealth += GetHealthChange;
+        }
+    }
+
+    void GetHealthChange()
+    {
+        text.text = playerHealth.CurrentHealth.ToString();
+    }
+}
