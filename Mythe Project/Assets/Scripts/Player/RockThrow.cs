24dce47790ec7604dea61900afc78f0a7eace0e4
26583 <you@example.com26583@ma-web.nl>
2020-05-13 12:13:09 +0200

    RockLift

---
+++ Mythe Project/Assets/Scripts/Player/RockThrow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class RockThrow : MonoBehaviour
+{
+    public event Action lifts;
+    public void LiftRocks()
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, 10,12);
+        for(int i = 0; i < hitColliders.Length; i++)
+        {
+            hitColliders[i].gameObject.GetComponent<RockBehavior>().AddToAction(this.GetComponent<RockThrow>());
+        }
+    }
+}
---
+++ Mythe Project/Assets/Scripts/Rock/RockBehavior.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockBehavior : MonoBehaviour
+{
+    Vector3 startPos;
+    float timer = 0;
+    void Start()
+    {
+        startPos = transform.position;
+    }
+    void GoUp()
+    {
+        timer += Time.deltaTime;
+        transform.position = Vector3.Lerp(startPos, startPos + Vector3.up *5, timer);
+    }
+    public void AddToAction(RockThrow rock)
+    {
+        rock.lifts += GoUp;
+    }
+}
