c0a83e844b5d0c4e7ce0f94f1c66fabe2586aa71
Wayne Hofstra <wayne.hofstra@hva.nl>
2020-05-13 22:56:27 +0200

    Added Weapon UI

--- Mythe Project/Assets/Scripts/UI/PlayerStatDisplay.cs
+++ Mythe Project/Assets/Scripts/UI/PlayerStatDisplay.cs
@@ -1,21 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
-public class PlayerStatDisplay : MonoBehaviour
+public class PlayerStatDisplay : UIscript
 {
-    Text text;
-    PlayerHealth playerHealth;
+    protected PlayerHealth playerHealth;
 
-    void Start()
+    protected new void Start()
     {
-        text = GetComponent<Text>();
+        base.Start();
+
         playerHealth = FindObjectOfType<PlayerHealth>();
         if (playerHealth != null)
         {
             playerHealth = playerHealth.GetComponent<PlayerHealth>();
-            playerHealth.changeHealth += GetHealthChange;
+            playerHealth.ChangeHealth += GetHealthChange;
         }
     }
 
