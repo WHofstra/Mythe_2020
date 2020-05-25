b7ea4dca24319f89f6c9d35bad2dbfb4e2d7be8c
Wayne Hofstra <wayne.hofstra@hva.nl>
2020-05-25 09:43:46 +0200

    Minor Changes

--- Mythe Project/Assets/Scripts/Dialogue/InGameDialogue.cs
+++ Mythe Project/Assets/Scripts/Dialogue/InGameDialogue.cs
@@ -5,5 +5,9 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Dialogue", menuName = "In-game Dialogue")]
 public class InGameDialogue : DialogueScene
 {
+    [SerializeField] protected bool _playOnLoop;
+    public bool PlayOnLoop { get { return _playOnLoop; } }
 
+    [SerializeField] protected float _repeatAfterSeconds;
+    public float RepeatSec { get { return _repeatAfterSeconds; } }
 }
